b5872d3786a8d31825541b0ab7d498e211365e7e
toppingart <toppingart123@gmail.com>
2023-01-11 18:53:27 -0700

    curl question 10 added

--- lab1.py
+++ lab1.py
@@ -7,3 +7,10 @@ print(requests.__version__)
 # 5. Modify your Python script to GET the Google homepage
 response = requests.get("https://google.com")
 # print(response.text)
+
+# Lab 1 - curl
+# 10. Modify your Python script so that it downloads itself...
+rawURL= "https://raw.githubusercontent.com/toppingart/CMPUT-404-Lab-1/main/lab1.py"
+response = requests.get(rawURL)
+print(response.text)
+
